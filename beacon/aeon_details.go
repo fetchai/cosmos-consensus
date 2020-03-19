e51f375d83ee567d5cad1136dbd9708eb8e72e28
jinmannwong <jinmannwong@gmail.com>
2020-03-19 11:37:13 +0000

    Add indices to log message

--- beacon/aeon_details.go
+++ beacon/aeon_details.go
@@ -32,7 +32,11 @@ func NewAeonDetails(
 			panic(fmt.Errorf("aeonDetails has DKG keys but not in validators"))
 		}
 		if !aeonKeys.CheckIndex(uint64(index)) {
-			panic(fmt.Errorf("aeonDetails has DKG keys not matching validator index"))
+			i := 0
+			for !aeonKeys.CheckIndex(uint64(i)) && i < validators.Size() {
+				i++
+			}
+			panic(fmt.Errorf("aeonDetails has DKG keys index %v not matching validator index %v", i, index))
 		}
 	}
 	ad := &aeonDetails{
