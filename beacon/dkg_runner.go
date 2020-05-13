de721cb85456d6e545635bf2f91b8996ced3eb67
jinmannwong <jinmannwong@gmail.com>
2020-05-13 09:25:18 +0100

    Make naming consistent

--- beacon/dkg_runner.go
+++ beacon/dkg_runner.go
@@ -35,14 +35,14 @@ type DKGRunner struct {
 	dkgCompletionCallback func(aeon *aeonDetails)
 	fastSync              bool
 
-	dhKey noise.DHKey
+	encryptionKey noise.DHKey
 
 	mtx sync.Mutex
 }
 
 // NewDKGRunner creates struct for starting new DKGs
 func NewDKGRunner(config *cfg.ConsensusConfig, chain string, db dbm.DB, val types.PrivValidator,
-	dhKey noise.DHKey, blockHeight int64) *DKGRunner {
+	encryptionKey noise.DHKey, blockHeight int64) *DKGRunner {
 	dkgRunner := &DKGRunner{
 		consensusConfig: config,
 		chainID:         chain,
@@ -53,7 +53,7 @@ func NewDKGRunner(config *cfg.ConsensusConfig, chain string, db dbm.DB, val type
 		aeonEnd:         -1,
 		completedDKG:    false,
 		fastSync:        false,
-		dhKey:           dhKey,
+		encryptionKey:   encryptionKey,
 	}
 	dkgRunner.BaseService = *cmn.NewBaseService(nil, "DKGRunner", dkgRunner)
 
@@ -202,7 +202,7 @@ func (dkgRunner *DKGRunner) startNewDKG(validatorHeight int64, validators *types
 	dkgRunner.Logger.Debug("startNewDKG: successful", "height", validatorHeight)
 	// Create new dkg that starts DKGResetDelay after most recent block height
 	dkgRunner.activeDKG = NewDistributedKeyGeneration(dkgRunner.consensusConfig, dkgRunner.chainID,
-		dkgRunner.privVal, dkgRunner.dhKey, validatorHeight, *validators, dkgRunner.aeonEnd, aeonLength)
+		dkgRunner.privVal, dkgRunner.encryptionKey, validatorHeight, *validators, dkgRunner.aeonEnd, aeonLength)
 	// Set logger with dkgID and node index for debugging
 	dkgLogger := dkgRunner.Logger.With("dkgID", dkgRunner.activeDKG.dkgID)
 	dkgLogger.With("index", dkgRunner.activeDKG.index())
