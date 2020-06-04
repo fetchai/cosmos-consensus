3b64a879401792a4e221e45fe389643120f7f3a4
nhutton <nathan.hutton@fetch.ai>
2020-06-04 16:34:42 +0100

    fix build failure

--- blockchain/v0/reactor.go
+++ blockchain/v0/reactor.go
@@ -339,6 +339,7 @@ FOR_LOOP:
 				// get the hash without persisting the state
 				var err error
 				state, err = bcR.blockExec.ApplyBlock(state, firstID, first)
+				if err != nil {
 					// TODO This is bad, are we zombie?
 					panic(fmt.Sprintf("V0 BCR Failed to process committed block (%d:%X): %v", first.Height, first.Hash(), err))
 				}
