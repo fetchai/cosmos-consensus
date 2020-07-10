b00a8c82dfba88a71749203d6aa8f84d28a200cf
jinmannwong <jinmannwong@gmail.com>
2020-07-10 18:44:58 +0100

    Don't use async

--- testnet/traders/run.sh
+++ testnet/traders/run.sh
@@ -12,7 +12,7 @@ do
         do
             rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 1000)
             echo "sending TX $rand_str, count $counter to $sub_word"
-            echo "curl -m 2 '$sub_word:26654/broadcast_tx_async?tx=\"rnd_$rand_str\"'"
+            echo "curl -m 2 '$sub_word:26654/broadcast_tx_sync?tx=\"rnd_$rand_str\"'"
             curl -m 2 "$sub_word:26654/broadcast_tx_sync?tx=\"rnd_$rand_str\""
 
             if [ $? -ne 0 ]
