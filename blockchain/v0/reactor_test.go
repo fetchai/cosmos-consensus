9be43946eed9808e439ca8d1aa11689c8459c9e5
jinmannwong <jinmannwong@gmail.com>
2020-07-15 14:00:56 +0100

    Fix blockchain test

--- blockchain/v0/reactor_test.go
+++ blockchain/v0/reactor_test.go
@@ -191,7 +191,9 @@ func TestBadBlockStopsPeer(t *testing.T) {
 
 	maxBlockHeight := int64(148)
 
-	otherChain := newBlockchainReactor(log.TestingLogger(), genDoc, privVals, maxBlockHeight)
+	// Other chain needs a different valiator set
+	otherGenDoc, otherPrivVals := randGenesisDoc(1, false, 30)
+	otherChain := newBlockchainReactor(log.TestingLogger(), otherGenDoc, otherPrivVals, maxBlockHeight)
 	defer func() {
 		otherChain.reactor.Stop()
 		otherChain.app.Stop()
