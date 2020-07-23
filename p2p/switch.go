9fcffab037a08f8a4c3be85f192da7ffe6374ec2
nhutton <nathan.hutton@fetch.ai>
2020-07-23 14:41:29 +0100

    remove redundant log

--- p2p/switch.go
+++ p2p/switch.go
@@ -331,9 +331,6 @@ func (sw *Switch) Peers() IPeerSet {
 // TODO: make record depending on reason.
 func (sw *Switch) StopPeerForError(peer Peer, reason interface{}) {
 	sw.Logger.Error("Stopping peer for error", "peer", peer, "err", reason)
-	defer func() {
-		sw.Logger.Error("Finished stopping peer for error", "peer", peer, "err", reason)
-	}()
 	sw.stopAndRemovePeer(peer, reason)
 
 	if peer.IsPersistent() {
