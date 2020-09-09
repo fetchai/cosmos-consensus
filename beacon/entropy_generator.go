86b01e249837f3ebb07177ff12184b159dddc6b1
jinmannwong <jinmannwong@gmail.com>
2020-09-09 12:04:26 +0100

    Reject invalid key files

--- beacon/entropy_generator.go
+++ beacon/entropy_generator.go
@@ -178,7 +178,12 @@ func (entropyGenerator *EntropyGenerator) LoadEntropyKeyFiles(db dbm.DB, privVal
 
 					if err1 == nil {
 						// Push the complete aeon into the entropy generator
-						aeonDetails = loadAeonDetails(aeonFile, vals, privValidator)
+						nextAeonDetails := loadAeonDetails(aeonFile, vals, privValidator)
+						if aeonDetails != nil && aeonDetails.End > nextAeonDetails.End {
+							return nil, fmt.Errorf("File %v contains out of order aeon keys. Previous aeon end %v, next aeon end %v",
+								fileToLoad, aeonDetails.End, nextAeonDetails.End)
+						}
+						aeonDetails = nextAeonDetails
 						entropyGenerator.nextAeons = append(entropyGenerator.nextAeons, aeonDetails)
 					} else {
 						return nil, errors.Wrap(err1, fmt.Sprintf("error loading validators for keyfile %v err: %v", fileToLoad, err1))
