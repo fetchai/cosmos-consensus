830db72bc31a19598a3ef636abfad1ead7f585c7
jinmannwong <jinmannwong@gmail.com>
2020-09-10 12:37:59 +0100

    Update version to v0.6.2

--- version/version.go
+++ version/version.go
@@ -22,7 +22,7 @@ const (
 	// XXX: Don't change the name of this variable or you will break
 	// automation :)
 
-	TMCoreSemVer     = "0.6.1"
+	TMCoreSemVer     = "0.6.2"
 	TMBaselineSemVer = "0.33.6"
 
 	// ABCISemVer is the semantic version of the ABCI library
